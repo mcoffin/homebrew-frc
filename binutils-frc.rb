4616f1549109cb3770dc6cc3393b140d1466ac11
Matt Coffin <mcoffin13@gmail.com>
2014-09-22 17:55:52 -0600

    Add SHA1 checksum for binutils-frc

--- binutils-frc.rb
+++ binutils-frc.rb
@@ -4,6 +4,7 @@ class BinutilsFrc < Formula
   homepage 'http://www.gnu.org/software/binutils/binutils.html'
   url 'http://ftpmirror.gnu.org/binutils/binutils-2.24.tar.gz'
   mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
+  sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
 
   def install
     target = 'arm-frc-linux-gnueabi'
